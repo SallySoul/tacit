d431f40d6a2e011bee5e8c79c74948ff35abed4a
SallySoul <sallysoul@protonmail.com>
2019-02-11 10:19:44 -0800

    Apply formatting

--- implicit-mesh/src/lib.rs
+++ implicit-mesh/src/lib.rs
@@ -6,10 +6,10 @@ extern crate serde_json;
 #[macro_use]
 mod util;
 
+pub mod cell_keys;
 pub mod function;
 pub mod function_ir;
 pub mod interval;
+pub mod mesh_tree;
 pub mod parser;
 pub mod parser_error;
-pub mod cell_keys;
-pub mod mesh_tree;
