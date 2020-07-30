2eda41f863894b3805ba135b7e9def48eda8e7b4
RussellBentley <russell.w.bentley@icloud.com>
2020-07-29 22:00:50 -0400

    Try updating wasm-bindgen

--- build_web.sh
+++ build_web.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 # Ensure wasm-bindgen in installed
-if ! (test -f $HOME/.cargo/bin/wasm-bindgen && test "`wasm-bindgen --version`" = "wasm-bindgen 0.2.33" ); then
+if ! (test -f $HOME/.cargo/bin/wasm-bindgen && test "`wasm-bindgen --version`" = "wasm-bindgen 0.2.67" ); then
     echo "wasm-bindgen not installed or out of date: installing"
-    cargo install -f wasm-bindgen-cli --version 0.2.33
+    cargo install -f wasm-bindgen-cli --version 0.2.67
 else
     echo "wasm-bindgen installed and up-to-date"
 fi
@@ -19,5 +19,5 @@ cp web-client/src_web/index.html target/web
 cargo build --release --target wasm32-unknown-unknown --manifest-path web-client/Cargo.toml
 
 # Build and Install web bindings
-wasm-bindgen target/wasm32-unknown-unknown/release/tacit_web_app.wasm --out-dir target/web --no-typescript --no-modules
+wasm-bindgen target/wasm32-unknown-unknown/release/tacit_web_app.wasm --out-dir target/web --no-typescript --target web
 
